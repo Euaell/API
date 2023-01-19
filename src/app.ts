2aa99a0b73b6232d56fce3bb003466c54217d18f
Euael Eshete <euaelmeko@gmail.com>
2023-01-19 19:46:15 +0300

    some dependencies

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,15 @@
 import express, { Request, Response, NextFunction } from "express"
 import { ErrorHandler } from "./middlewares/ErrorHandler"
+import morgan from "morgan"
+import cors from "cors"
+
 
 const app = express()
+
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(express.json())
+app.use(morgan(process.env.NODE_ENV === "production" ? "combined" : "dev"))
+app.use(cors())
 
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
     res.status(200).json({ message: "Hello World!" })
